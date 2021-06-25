a892dfeea10a38495094f75945237d5af866977c
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2021-06-26 00:09:42 +0300

    Create io.cc

---
+++ chapter1/3/io.cc
@@ -0,0 +1,13 @@
+// Отримати число
+int getNumber() {
+  std::cout << "> ";
+  int number;
+  std::cin >> number;
+  return number;
+}
+
+// Вивести результат
+void printResult(int result) {
+  std::cout << "Result: " << result << '\n';
+  std::cout << '\n';
+}
