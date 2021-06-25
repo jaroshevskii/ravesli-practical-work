8bbdec4591ce6de2b2bdac483276491b1d6bbe39
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2021-06-26 00:04:40 +0300

    Create app.cc

---
+++ chapter1/3/app.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "io.h"
+
+int main() {
+  // Отримую перше число
+  std::cout << "Enter first number\n";
+  int number1 = getNumber();
+  
+  // отимую друге число
+  std::cout << "Enter second number\n";
+  int number2 = getNumber();
+  std::cout << '\n';
+  
+  // Вивожу результат складоння двох чисел
+  printResult(number1 + number2);
+  return 0;
+}
