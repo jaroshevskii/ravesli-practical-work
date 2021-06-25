1842f66939e98a4ad363fec3ef9cf9297ae0a43b
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2021-06-25 23:49:56 +0300

    Update app.cc

--- chapter1/1/app.cc
+++ chapter1/1/app.cc
@@ -1,1 +1,26 @@
+#include <iostream>
 
+int getNumber() {
+  std::cout << "> number = ";
+  int x;
+  std::cin >> x;
+  return x;
+}
+
+void printResult(int x) {
+  std::cout << "Result: " << x << '\n';
+  std::cout << '\n';
+}
+
+int main() {
+  // Отримую перше число
+  int number1 = getNumber();
+  
+  // отимую друге число
+  int number2 = getNumber();
+  std::cout << '\n';
+  
+  // Вивожу результат складоння двох чисел
+  printResult(number1 + number2);
+  return 0;
+}
